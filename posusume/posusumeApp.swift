2c5052930b2fffde0264a3e8ea04cce09916318c
bannzai <kingkong999yhirose@gmail.com>
2021-03-28 22:09:21 +0900

    :tada: Initial commit

---
+++ posusume/posusumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  posusumeApp.swift
+//  posusume
+//
+//  Created by Yudai.Hirose on 2021/03/28.
+//
+
+import SwiftUI
+
+@main
+struct posusumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
