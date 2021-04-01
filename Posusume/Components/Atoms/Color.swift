7c67bd56341a8f18edae6f5da5830ef823e9f7cf
bannzai <kingkong999yhirose@gmail.com>
2021-04-01 22:20:34 +0900

    :art: add barn color

---
+++ Posusume/Components/Atoms/Color.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+extension Color {
+    // #FFAB8B
+    static let barnEnd: Color = Color(red: 255 / 255, green: 171 / 255, blue: 139 / 255)
+    // #E95468
+    static let barnStart: Color = Color(red: 233 / 255, green: 84 / 255, blue: 104 / 255)
+}
