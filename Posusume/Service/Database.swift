6148ad0d2f69a1fec1eb9c779bdd7f8c803a229d
bannzai <kingkong999yhirose@gmail.com>
2021-04-10 23:42:36 +0900

    :sparkels: add database service

---
+++ Posusume/Service/Database.swift
@@ -0,0 +1,35 @@
+import Foundation
+import Combine
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+private let database = Firestore.firestore()
+struct Database {
+    static let shared = Database()
+    private init() { }
+    
+    func fetch<T: Decodable>(path: DatabaseDocumentPathBuilder<T>) -> AnyPublisher<T, Error> {
+        database.document(path.path).get()
+    }
+    func fetch<T: Decodable>(path: DatabaseCollectionPathBuilder<T>) -> AnyPublisher<CollectionMapper<T>, Error> {
+        Future { promise in
+            database.collection(path.path).getDocuments { (snapshot, error) in
+                if let error = error {
+                    return promise(.failure(error))
+                }
+                if let snapshot = snapshot {
+                    return promise(.success(.init(snapshots: snapshot.documents)))
+                }
+                return promise(.success(.init(snapshots: [])))
+            }
+        }.eraseToAnyPublisher()
+    }
+    
+    func create<T: Encodable>(value: T, path: DatabaseCollectionPathBuilder<T>) -> AnyPublisher<T, Error> {
+        database.collection(path.path).add(value: value)
+    }
+
+    func update<T: Encodable>(value: T, path: DatabaseDocumentPathBuilder<T>) -> AnyPublisher<T, Error> {
+        database.document(path.path).set(from: value)
+    }
+}
