144a8e0cf1ed3d5b13469606e0d242ffdd072818
bannzai <kingkong999yhirose@gmail.com>
2021-04-10 17:39:12 +0900

    :recycle: using Auth

--- Posusume/Service/Auth.swift
+++ Posusume/Service/Auth.swift
@@ -2,14 +2,14 @@ import Foundation
 import FirebaseAuth
 
 protocol AuthProtocol {
-    func auth(closure: @escaping (Result<Void, Swift.Error>) -> Void)
-    func fetchUserID() -> String?
+    func auth(closure: @escaping (Result<UserID, Swift.Error>) -> Void)
+    func fetchUserID() -> UserID?
 }
 
 struct Auth: AuthProtocol {
     fileprivate init() { }
-    
-    func auth(closure: @escaping (Result<Void, Swift.Error>) -> Void) {
+
+    func auth(closure: @escaping (Result<UserID, Swift.Error>) -> Void) {
         FirebaseAuth.Auth.auth().signInAnonymously() { (result, error) in
             if let error = error {
                 closure(.failure(error))
@@ -18,22 +18,26 @@ struct Auth: AuthProtocol {
             guard let result = result else {
                 fatalError("unexpected pattern about result and error is nil")
             }
-            self.store(userID: result.user.uid)
-            closure(.success(()))
+            let userID = UserID(rawValue: result.user.uid)!
+            self.store(userID: userID)
+            closure(.success(userID))
         }
     }
     
     private enum StoreKey {
         static let firebaseUserID: String = "firebaseUserID"
     }
-    private func store(userID: String) {
+    private func store(userID: UserID) {
         guard UserDefaults.standard.string(forKey: StoreKey.firebaseUserID) == nil else {
             return
         }
-        UserDefaults.standard.setValue(userID, forKey: StoreKey.firebaseUserID)
+        UserDefaults.standard.setValue(userID.rawValue, forKey: StoreKey.firebaseUserID)
     }
-    func fetchUserID() -> String? {
-        UserDefaults.standard.string(forKey: StoreKey.firebaseUserID)
+    func fetchUserID() -> UserID? {
+        guard let userID = UserDefaults.standard.string(forKey: StoreKey.firebaseUserID) else {
+            return nil
+        }
+        return UserID(rawValue: userID)
     }
 }
 
