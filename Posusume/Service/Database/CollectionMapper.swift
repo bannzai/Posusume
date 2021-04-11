1ef7b315df07a010bf0d54029f01712a17a55ded
bannzai <kingkong999yhirose@gmail.com>
2021-04-11 09:46:40 +0900

    :recycle: remove unnecessary function and replace to more smart syntax

--- Posusume/Service/Database/CollectionMapper.swift
+++ Posusume/Service/Database/CollectionMapper.swift
@@ -3,7 +3,7 @@ import FirebaseFirestore
 
 struct CollectionMapper<Entity: Decodable> {
     let snapshots: [DocumentSnapshot]
-    func mapped() throws -> [Entity] { try snapshots.map { try $0.data(as: Entity.self) }.compactMap { $0 }}
-    func compacted() -> [Entity] { snapshots.compactMap { try? $0.data(as: Entity.self) }}
+
+    var compacted: [Entity] { snapshots.compactMap { try? $0.data(as: Entity.self) }}
 }
 
