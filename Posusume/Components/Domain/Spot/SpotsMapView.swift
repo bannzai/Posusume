bc5caef2e4f01f7d07922c71900daaf848c1f038
bannzai <kingkong999yhirose@gmail.com>
2021-04-14 01:15:38 +0900

    :recycle: WIP fix map view

--- Posusume/Components/Domain/Spot/SpotsMapView.swift
+++ Posusume/Components/Domain/Spot/SpotsMapView.swift
@@ -4,39 +4,57 @@ import ComposableArchitecture
 import MapKit
 
 struct SpotMapState: Equatable {
+    var center: CLLocationCoordinate2D = .init(latitude: 35.655164046, longitude: 139.740663704)
+    var span: CoordinateSpan = .init(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    var region: MKCoordinateRegion { .init(center: center, span: .init(latitudeDelta: span.latitudeDelta, longitudeDelta: span.longitudeDelta)) }
     var spots: [Spot] = []
     var error: EquatableError?
+
+    var spotListState = SpotListState()
 }
 
 enum SpotMapAction: Equatable {
-    case fetch(latitude: CLLocationDegrees, longitude: CLLocationDegrees)
+    case regionChange(center: CLLocationCoordinate2D, span: CoordinateSpan)
+    case fetch
     case fetched(Result<[Spot], EquatableError>)
 }
 
-let spotMapReducer = Reducer<SpotMapState, SpotMapAction, SpotMapEnvironment> { (state, action, environment) in
-    struct Canceller: Hashable { }
-    switch action {
-    case let .fetch(latitude, longitude):
-        let offset: CLLocationDegrees = 3
-        let pathBuilder = DatabaseCollectionPathBuilder<Spot>.spots(args: [
-            (.latitude, .lessOrEqual(latitude + offset)),
-            (.longitude, .lessOrEqual(longitude + offset)),
-            (.latitude, .greaterOrEqual(latitude - offset)),
-            (.longitude, .greaterOrEqual(longitude - offset)),
-        ])
-        return environment.fetchList(pathBuilder)
-            .mapError(EquatableError.init(error:))
-            .receive(on: environment.mainQueue)
-            .catchToEffect()
-            .map(SpotMapAction.fetched)
-    case .fetched(.success(let spots)):
-        state.spots = spots
-        return .none
-    case .fetched(.failure(let error)):
-        state.error = error
-        return .none
-    }
-}
+let spotMapReducer = Reducer<SpotMapState, SpotMapAction, SpotMapEnvironment>.combine(
+    .init { (state, action, environment) in
+        struct Canceller: Hashable { }
+        switch action {
+        case let .regionChange(center, meters):
+            return .none
+        case .fetch:
+            // TODO: adjustment offset from span
+            let offset: CLLocationDegrees = 3
+            let latitude = state.center.latitude
+            let longitude = state.center.longitude
+            let pathBuilder = DatabaseCollectionPathBuilder<Spot>.spots(args: [
+                (.latitude, .lessOrEqual(latitude + offset)),
+                (.longitude, .lessOrEqual(longitude + offset)),
+                (.latitude, .greaterOrEqual(latitude - offset)),
+                (.longitude, .greaterOrEqual(longitude - offset)),
+            ])
+            return environment.fetchList(pathBuilder)
+                .mapError(EquatableError.init(error:))
+                .receive(on: environment.mainQueue)
+                .catchToEffect()
+                .map(SpotMapAction.fetched)
+        case .fetched(.success(let spots)):
+            state.spots = spots
+            return .none
+        case .fetched(.failure(let error)):
+            state.error = error
+            return .none
+        }
+    },
+    spotListReducer.pullback(
+        state: \.spotListState,
+        action: <#T##CasePath<GlobalAction, SpotListAction>#>,
+        environment: <#T##(GlobalEnvironment) -> SpotListEnvironment#>
+    )
+)
 
 struct SpotMapEnvironment {
     let me: Me
@@ -45,14 +63,15 @@ struct SpotMapEnvironment {
 }
 
 struct SpotMapView: View {
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 35.655164046, longitude: 139.740663704), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
-
     let store: Store<SpotMapState, SpotMapAction>
     var body: some View {
         WithViewStore(store) { viewStore in
             ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
-                Map(coordinateRegion: viewStore.binding(get: ))
-                Map(coordinateRegion: $region)
+                Map(coordinateRegion: viewStore.binding(
+                        get: \.region,
+                        send: { .regionChange(center: $0.center, span: .init(latitudeDelta: $0.span.longitudeDelta, longitudeDelta: $0.span.longitudeDelta))
+                        }
+                ))
                 ZStack {
                     BarnBottomSheet()
                     SpotList(
@@ -70,7 +89,7 @@ struct SpotMapView: View {
                     .padding()
                 }
                 .frame(width: UIScreen.main.bounds.width, height: BarnBottomSheet.height, alignment: .bottom)
-                .onAppear { viewStore.send(.fetch(latitude: region.center.latitude, longitude: region.center.longitude))}
+                .onAppear { viewStore.send(.fetch) }
             }
         }
     }
@@ -87,7 +106,7 @@ struct SpotMapView_Previews: PreviewProvider {
                     ],
                     error: nil
                 ),
-                reducer: SpotMapReducer,
+                reducer: spotMapReducer,
                 environment: SpotMapEnvironment(
                     me: .init(id: Me.ID(rawValue: "1")),
                     fetchList: { _ in Future(value: spots).eraseToAnyPublisher() },
@@ -95,7 +114,6 @@ struct SpotMapView_Previews: PreviewProvider {
                 )
             )
         )
-        .previewLayout(.fixed(width: UIScreen.main.bounds.width, height: 300))
     }
     static var spots: [Spot] = (0..<10).map {
         .init(id: SpotID(rawValue: "identifier\($0)"), latitude: 100, longitude: 100, name: "spot \($0)", imagePath: nil)
