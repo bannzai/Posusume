62a30a5d87c2882cb6907c6cc8455222db975087
bannzai <kingkong999yhirose@gmail.com>
2021-04-16 10:09:06 +0900

    :sparkels: combined reducer
    #
    # ==== Emojis ====
    # 🐛  :bug: バグ修正
    # 🤘  :metal: 機能改善
    # 🐚  :shell: コマンドを実行した結果とか
    # ✨  :sparkles: 部分的な機能追加
    # 🎉  :tada: 盛大に祝うべき大きな機能追加
    # 💉  :syringe: libraryの追加
    # 📝  :memo: ドキュメント・READMEの整備
    # ♻️   :recycle: リファクタリング
    # 🚿  :shower: 不要な機能・使われなくなった機能の削除
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # 🆙  :up: 依存パッケージなどのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🎨  :art: デザイン修正
    # 🚧  :construction: WIP 一時的なコミット
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # reference from http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/
    # for https://github.com/bannzai/commit_template

--- Posusume/RootView.swift
+++ Posusume/RootView.swift
@@ -3,27 +3,79 @@ import MapKit
 import ComposableArchitecture
 import Combine
 
-struct RootState: Equatable {
-    var spots: [Spot] = []
+struct RootState {
+    var login: LoginState? = .init()
+    var spots: SpotMapState?
     var error: EquatableError?
+
+    mutating func authorized(me: Me) {
+        login = nil
+        error = nil
+        spots = .init()
+    }
 }
 
 enum RootAction: Equatable {
-    case onAppear
-    case fetch
-    case fetched(Result<[Spot], EquatableError>)
+    case login(LoginAction)
+    case spots(SpotMapAction)
 }
 
 struct RootEnvironment {
     let auth: Auth
-    var mainQueue: AnySchedulerOf<DispatchQueue>
+    let mainQueue: AnySchedulerOf<DispatchQueue>
 }
 
-struct RootView: View {
+let rootReducer = Reducer<RootState, RootAction, RootEnvironment>.combine(
+    loginReducer.optional().pullback(
+        state: \.login,
+        action: /RootAction.login,
+        environment: { root in
+            LoginEnvironment(auth: root.auth, mainQueue: root.mainQueue, createOrUpdateUser: FirestoreDatabase.shared.createOrUpdate)
+        }
+    ),
+    spotMapReducer.optional().pullback(
+        state: \.spots,
+        action: /RootAction.spots,
+        environment: { (environment: RootEnvironment) in
+            return SpotMapEnvironment(
+                me: authorized.authorized(),
+                fetchList: FirestoreDatabase.shared.fetchList,
+                watchList: FirestoreDatabase.shared.watchList,
+                mainQueue: environment.mainQueue
+            )
+        }
+    ),
+    .init { (state, action, environment) in
+        switch action {
+        case .spots:
+            return .none
+        case let .login(.endLogin(me)):
+            state.authorized(me: me)
+            return .none
+        case .login:
+            return .none
+        }
+    }
+)
+    
 
-    var body: some View {
-        EmptyView()
-            .edgesIgnoringSafeArea(.all)
+
+struct RootView: View {
+    let store: Store<RootState, RootAction> = .init(
+        initialState: .init(),
+        reducer: rootReducer,
+        environment: RootEnvironment(auth: auth, mainQueue: .main)
+    )
+    @ViewBuilder public var body: some View {
+        IfLetStore(self.store.scope(state: { $0.login }, action: RootAction.login)) { store in
+            LoginView(store: store)
+        }
+        .edgesIgnoringSafeArea(.all)
+
+        IfLetStore(self.store.scope(state: { $0.spots }, action: RootAction.spots)) { store in
+            SpotMapView(store: store)
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
