2477a02738ba6e1b717637b3716e5185db53d957
bannzai <kingkong999yhirose@gmail.com>
2021-04-19 08:03:58 +0900

    :recycle: Integrate to PhotoLibraryView

--- Posusume/Components/Domain/Photo/PhotoLibraryView.swift
+++ Posusume/Components/Domain/Photo/PhotoLibraryView.swift
@@ -1,8 +1,113 @@
-import SwiftUI
+import Foundation
 import UIKit
-import Photos
+import SwiftUI
 import PhotosUI
+import Photos
+import ComposableArchitecture
 import Combine
+
+struct PhotoLibraryState: Equatable {
+    var selectedImage: UIImage?
+    var isPresentOpenSettingAppAlert: Bool
+    var isPresentNotPermissionAlert: Bool
+    var error: EquatableError?
+}
+
+enum PhotoLibraryAction: Equatable {
+    case prepare
+    case authorized(Result<PHAuthorizationStatus, Never>)
+    case selected([PHPickerResult])
+    case converted(Result<PhotoLibraryResult, EquatableError>)
+    case end(PhotoLibraryResult)
+    case presentOpenSettingAlert
+    case openSetting
+    case presentedOpenSetting
+    case presentNotPermissionAlert
+    case confirmedNotPermission
+    case cancelAlertAction
+}
+
+struct PhotoLibraryEnvironment {
+    let me: Me
+    let photoLibrary: PhotoLibrary
+    let mainQueue: AnySchedulerOf<DispatchQueue>
+}
+
+let reducer: Reducer<PhotoLibraryState, PhotoLibraryAction, PhotoLibraryEnvironment> = .init { state, action, environment in
+    switch action {
+    case .prepare:
+        switch environment.photoLibrary.prepareActionType() {
+        case nil:
+            return .none
+        case .openSettingApp:
+            return Effect(value: .presentOpenSettingAlert)
+        case .requestAuthorization:
+            return environment
+                .photoLibrary
+                .requestAuthorization()
+                .receive(on: environment.mainQueue)
+                .catchToEffect()
+                .map(PhotoLibraryAction.authorized)
+        }
+    case let .authorized(.success(status)):
+        switch status {
+        case .authorized:
+            return .none
+        case .limited:
+            return .none
+        case .notDetermined:
+            return Effect(value: .presentNotPermissionAlert)
+        case .denied:
+            return Effect(value: .presentNotPermissionAlert)
+        case .restricted:
+            return Effect(value: .presentNotPermissionAlert)
+        @unknown default:
+            assertionFailure("unexpected authorization status \(status):\(status.rawValue)")
+            return .none
+        }
+    case let .selected(selectedResults):
+        guard let selected = selectedResults.first else {
+            fatalError("unexpected selectedResult is empty")
+        }
+        return environment
+            .photoLibrary
+            .convert(pickerResult: selected)
+            .mapError(EquatableError.init(error:))
+            .receive(on: environment.mainQueue)
+            .catchToEffect()
+            .map(PhotoLibraryAction.converted)
+    case let .converted(.success(result)):
+        return Effect(value: .end(result))
+    case let .converted(.failure(error)):
+        state.error = error
+        return .none
+    case .end:
+        return .none
+    case .presentOpenSettingAlert:
+        state.isPresentOpenSettingAppAlert = true
+        return .none
+    case .openSetting:
+        guard UIApplication.shared.canOpenURL(URL(string: UIApplication.openSettingsURLString)!) else {
+            assertionFailure("unexpected cannot open setting apps")
+            return .none
+        }
+        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
+        return Effect(value: .presentedOpenSetting)
+    case .presentedOpenSetting:
+        state.isPresentOpenSettingAppAlert = false
+        return .none
+    case .presentNotPermissionAlert:
+        state.isPresentNotPermissionAlert = true
+        return .none
+    case .confirmedNotPermission:
+        state.isPresentNotPermissionAlert = false
+        return .none
+    case .cancelAlertAction:
+        state.isPresentOpenSettingAppAlert = false
+        state.isPresentNotPermissionAlert = false
+        return .none
+    }
+}
 
 private let sharedPhotoLibraryConfiguration: PHPickerConfiguration = .init(photoLibrary: PHPhotoLibrary.shared())
 
