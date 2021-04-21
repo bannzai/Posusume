5ed04ca81164a5e257e9df66dd9980ae199a39e4
bannzai <kingkong999yhirose@gmail.com>
2021-04-22 00:21:50 +0900

    :art: add mock image

--- Posusume/Preview Content/Mock/MockPhotoLibrary.swift
+++ Posusume/Preview Content/Mock/MockPhotoLibrary.swift
@@ -6,6 +6,6 @@ import Combine
 struct MockPhotoLibrary: PhotoLibrary {
     func prepareActionType() -> PhotoLibraryPrepareAction? { nil }
     func requestAuthorization() -> AnyPublisher<PHAuthorizationStatus, Never> { Future(value: .authorized).eraseToAnyPublisher() }
-    func convert(pickerResult: PHPickerResult) -> AnyPublisher<PhotoLibraryResult, Error> { Future(value: PhotoLibraryResult(image: .init(), location: nil, takeDate: nil)).eraseToAnyPublisher() }
+    func convert(pickerResult: PHPickerResult) -> AnyPublisher<PhotoLibraryResult, Error> { Future(value: PhotoLibraryResult(image: UIImage(named: "hanahana")!, location: nil, takeDate: nil)).eraseToAnyPublisher() }
 }
 #endif
