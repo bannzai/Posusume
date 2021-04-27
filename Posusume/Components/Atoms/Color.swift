3ad8b19944f07451f7e01b2db875cb6a5a4c02e0
bannzai <kingkong999yhirose@gmail.com>
2021-04-27 09:32:57 +0900

    :recycle: Refactor gradient colors

--- Posusume/Components/Atoms/Color.swift
+++ Posusume/Components/Atoms/Color.swift
@@ -1,16 +1,20 @@
 import SwiftUI
 
 extension Color {
-    // #FFAB8B
-    static let barnEnd: Color = Color(red: 255 / 255, green: 171 / 255, blue: 139 / 255)
-    // #E95468
-    static let barnStart: Color = Color(red: 233 / 255, green: 84 / 255, blue: 104 / 255)
     // #F8F8F8
     static let screenBackground: Color = Color(red: 248 / 255, green: 248 / 255, blue: 248 / 255)
     // #EE6770
     static let appPrimary: Color = Color(red: 239 / 255, green: 103 / 255, blue: 112 / 255)
     // #CBCBCB
     static let placeholder: Color = Color(white: 203 / 255)
+    
+}
+
+fileprivate extension Color {
+    // #FFAB8B
+    static let barnEnd: Color = Color(red: 255 / 255, green: 171 / 255, blue: 139 / 255)
+    // #E95468
+    static let barnStart: Color = Color(red: 233 / 255, green: 84 / 255, blue: 104 / 255)
 }
 
 struct GradientColor {
@@ -19,9 +23,11 @@ struct GradientColor {
         startPoint: .bottom,
         endPoint: .top
     )
-    static let lower = LinearGradient(
-            gradient: Gradient(colors: [.barnStart, Color.barnEnd]),
-            startPoint: .bottom,
-            endPoint: .top
-        )
+
+    static let lower = barn
+    static let barn = LinearGradient(
+        gradient: Gradient(colors: [.barnStart, Color.barnEnd]),
+        startPoint: .bottom,
+        endPoint: .top
+    )
 }
