6095a7127203f83831cc52b5f678a7a3c2ce02f0
bannzai <kingkong999yhirose@gmail.com>
2021-05-04 17:35:55 +0900

    :recycle: Rename to LocationSelectView

--- Posusume/Components/Domain/Location/LocationSelectView.swift
+++ Posusume/Components/Domain/Location/LocationSelectView.swift
@@ -4,28 +4,30 @@ import ComposableArchitecture
 import MapKit
 import CoreLocation
 
-struct LocationSearchState: Equatable {
+struct LocationSelectState: Equatable {
     var searchText: String = ""
     var marks: [PlaceMark] = []
+    var selected: PlaceMark? = nil
 }
 
-enum LocationSearchAction: Equatable {
+enum LocationSelectAction: Equatable {
     case search(String)
     case searched(Result<[PlaceMark], EquatableError>)
+    case selected(PlaceMark)
 }
 
-struct LocationSearchEnvironment {
+struct LocationSelectEnvironment {
     let geocoder: Geocoder
 }
 
-let locationSearchReducer: Reducer<LocationSearchState, LocationSearchAction, LocationSearchEnvironment> = .init { state, action, environment in
+let locationSelectReducer: Reducer<LocationSelectState, LocationSelectAction, LocationSelectEnvironment> = .init { state, action, environment in
     switch action {
     case let .search(text):
         state.searchText = text
         return Effect(environment.geocoder.geocoder(address: text))
             .mapError(EquatableError.init(error:))
             .catchToEffect()
-            .map(LocationSearchAction.searched)
+            .map(LocationSelectAction.searched)
             .eraseToEffect()
     case let .searched(.success(marks)):
         state.marks = []
@@ -33,11 +35,14 @@ let locationSearchReducer: Reducer<LocationSearchState, LocationSearchAction, Lo
     case .searched(.failure):
         state.marks = []
         return .none
+    case let .selected(mark):
+        state.selected = mark
+        return .none
     }
 }
 
-struct LocationSearchView: View {
-    let store: Store<LocationSearchState, LocationSearchAction>
+struct LocationSelectView: View {
+    let store: Store<LocationSelectState, LocationSelectAction>
     var body: some View {
         WithViewStore(store) { viewStore in
             ScrollView {
@@ -56,6 +61,9 @@ struct LocationSearchView: View {
                     ForEach(0..<viewStore.marks.count) { i in
                         HStack {
                             Text(formatForLocation(mark: viewStore.marks[i]))
+                                .onTapGesture {
+                                    viewStore.send(.selected(viewStore.marks[i]))
+                                }
                         }
                         .padding(.horizontal, 20)
                     }
@@ -72,13 +80,13 @@ func formatForLocation(mark: PlaceMark) -> String {
     return mark.address.address
 }
 
-struct LocationSearchView_Previews: PreviewProvider {
+struct LocationSelectView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationSearchView(
+        LocationSelectView(
             store: .init(
                 initialState: .init(),
-                reducer: locationSearchReducer,
-                environment: LocationSearchEnvironment(
+                reducer: locationSelectReducer,
+                environment: LocationSelectEnvironment(
                     geocoder: geocoder
                 )
             )
