550ec24cb8c3e738a6b6b2c048886c5b4ec68b54
bannzai <kingkong999yhirose@gmail.com>
2021-07-31 18:48:39 +0900

    :sparkles: Implement introspection

---
+++ scripts/apollo/ApolloCodegen/Sources/ApolloCodegen/Introspection.swift
@@ -0,0 +1,18 @@
+import Foundation
+import ApolloCodegenLib
+
+func introspection() throws {
+    guard let introspectionURLString = ProcessInfo.processInfo.environment["POSUSUME_GRAPHQL_API_INTROSPECTION_URL"],
+          let introspectionURL = URL(string: introspectionURLString) else {
+        fatalError("Unexpected POSUSUME_GRAPHQL_API_INTROSPECTION_URL is empty or invalid URL")
+    }
+    print("Put schema file to \(introspectionURL.absoluteString)")
+    
+    try ApolloSchemaDownloader.run(
+        with: cliPath,
+        options: .init(
+            downloadMethod: .introspection(endpointURL: introspectionURL),
+            outputFolderURL: schemaPath
+        )
+    )
+}
