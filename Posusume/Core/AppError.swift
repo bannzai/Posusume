6dfbf9ce9ac638a2767a4809aa0b100756b9e004
bannzai <kingkong999yhirose@gmail.com>
2021-09-21 22:46:36 +0900

    :memo: Defined AppError

---
+++ Posusume/Core/AppError.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+public enum AppError: Error, LocalizedError {
+    case requireLogin
+    case userDeleted
+    case unstableNetwork
+    case fatal
+    case support(error: Error, code: Int, domain: String, userInfo: [String: Any])
+
+    public var errorDescription: String? {
+        switch self {
+        case .requireLogin:
+            return "認証期限切れです。再ログインしてください"
+        case .userDeleted:
+            return "ユーザーが既に削除されています"
+        case .unstableNetwork:
+            return "ネットワークが不安定です。通信環境をお確かめのうえ再度お試しください"
+        case .fatal:
+            return "不正なリクエストです"
+        case let .support(error, code, domain, userInfo):
+            return """
+                    不明なエラーが発生しました。下記の情報を添えてサポートまでお問い合わせください。
+                    error: \(error)
+                    code: \(code),
+                    domain: \(domain)
+                    userInfo: \(userInfo)
+                    """
+        }
+    }
+}
