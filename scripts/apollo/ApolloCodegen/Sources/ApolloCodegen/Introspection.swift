a2e4f34f7e49247c9d61da8c6948f14975a32670
bannzai <kingkong999yhirose@gmail.com>
2021-09-21 22:59:17 +0900

    :memo: Remove unnecessary fields

--- scripts/apollo/ApolloCodegen/Sources/ApolloCodegen/Introspection.swift
+++ scripts/apollo/ApolloCodegen/Sources/ApolloCodegen/Introspection.swift
@@ -8,15 +8,10 @@ func downloadSchema() throws {
     }
     print("Put schema file from \(introspectionURLString) to \(schemaPath.absoluteString)")
     
-    guard let adminSecret = ProcessInfo.processInfo.environment["HASURA_ADMIN_SECRET"] else {
-        fatalError("downloadSchema necessary HASURA_ADMIN_SECRET")
-    }
-
     try ApolloSchemaDownloader.run(
         with: cliPath,
         options: ApolloSchemaOptions(
             downloadMethod: .introspection(endpointURL: introspectionURL),
-            headers: ["X-Hasura-Admin-Secret: \(adminSecret)"],
             outputFolderURL: schemaPath.deletingLastPathComponent(),
             downloadTimeout: 30
         )
