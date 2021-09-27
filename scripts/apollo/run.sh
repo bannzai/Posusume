cab6796d6e2bd316d55987366a3d61d83b585a58
bannzai <kingkong999yhirose@gmail.com>
2021-09-27 21:18:52 +0900

    :memo: fix paths

--- scripts/apollo/run.sh
+++ scripts/apollo/run.sh
@@ -4,8 +4,9 @@ set -eu
 echo 'Start `Apollo`'
 
 SCRIPT_DIR="$(cd `dirname $0` && pwd -P)"
+APOLLO_CODGEN_DIR="$(cd $SCRIPT_DIR && cd ApolloCodegen && pwd -P)"
 
 # ApolloCodegen uses relative paths. So, move the path
-cd $SCRIPT_DIR
+cd $APOLLO_CODGEN_DIR
 xcrun -sdk macosx swift run ApolloCodegen 
 cd -
