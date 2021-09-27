b9ae853e3b2229ca7a9a5728cf7ed0f6ddefddd0
bannzai <kingkong999yhirose@gmail.com>
2021-09-27 21:20:51 +0900

    :up: version scripts
    Got Error
    al<UnsafeMutablePointer<Int8>>') must be unwrapped to a value of type 'UnsafeMutablePointer<CChar>' (aka 'UnsafeMutablePointer<Int8>')
          free(cPattern)
               ^
    /Users/bannzai/ghq/github.com/bannzai/Posusume/scripts/apollo/ApolloCodegen/.build/checkouts/PathKit/Sources/PathKit.swift:583:12: note: coalesce using '??' to provide a default when the optional value contains 'nil'
          free(cPattern)
               ^
                        ?? <#default value#>
    /Users/bannzai/ghq/github.com/bannzai/Posusume/scripts/apollo/ApolloCodegen/.build/checkouts/PathKit/Sources/PathKit.swift:583:12: note: force-unwrap using '!' to abort execution if the optional value contains 'nil'
          free(cPattern)
               ^
                       !
    [7/9] Compiling ApolloUtils ApolloExtension.swift

--- scripts/apollo/ApolloCodegen/Package.swift
+++ scripts/apollo/ApolloCodegen/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         .package(name: "Apollo",
                  url: "https://github.com/apollographql/apollo-ios.git",
-                 .upToNextMinor(from: "0.45.0"))
+                 .upToNextMinor(from: "0.49.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
