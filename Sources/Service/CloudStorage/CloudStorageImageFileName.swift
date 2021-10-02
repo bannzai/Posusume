977bbd59fea3b1c2aa0cdb0d8995e24b34870210
bannzai <kingkong999yhirose@gmail.com>
2021-10-03 08:43:22 +0900

    :recycle: Remove unnecessary code

--- Sources/Service/CloudStorage/CloudStorageImageFileName.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-public protocol CloudStorageImageFileName {
-    var imageFileName: String { get }
-}
--- Sources/Service/CloudStorage/CloudStoragePathBuilder.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-public struct CloudStoragePathBuilder<Entity: CloudStorageImageFileName> {
-    let initialFileName: String
-}
