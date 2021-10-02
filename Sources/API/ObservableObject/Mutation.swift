08deade068a35c21d23f4bf9671994d91bc27f79
bannzai <kingkong999yhirose@gmail.com>
2021-10-03 07:48:43 +0900

    :sparkels: add Mutation

---
+++ Sources/API/ObservableObject/Mutation.swift
@@ -0,0 +1,35 @@
+import Foundation
+import Apollo
+
+@MainActor
+public final class Mutation<Mutation: GraphQLMutation>: ObservableObject {
+    @Published public private(set) var isProcessing = false
+    @Published public private(set) var data: Mutation.Data?
+    @Published public private(set) var error: Error?
+
+    internal func perform(mutation: Mutation) async {
+        isProcessing = true
+        defer {
+            isProcessing = false
+        }
+
+        do {
+            data = try await AppApolloClient.shared.perform(mutation: mutation)
+        } catch {
+            self.error = error
+        }
+    }
+
+    internal func perform<Query: GraphQLQuery>(mutation: Mutation, queryAfterPerform query: Query) async {
+        await perform(mutation: mutation)
+        _ = try? await AppApolloClient.shared.fetchFromServer(query: query)
+    }
+
+    public func callAsFunction(_ mutation: Mutation) async {
+        await perform(mutation: mutation)
+    }
+
+    public func callAsFunction<Query: GraphQLQuery>(_ mutation: Mutation, queryAfterPerform query: Query) async {
+        await perform(mutation: mutation, queryAfterPerform: query)
+    }
+}
