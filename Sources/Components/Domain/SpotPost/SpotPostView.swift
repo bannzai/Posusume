00eb7d76d224c51a9fa3784ca92fc1d8cf6bde93
bannzai <kingkong999yhirose@gmail.com>
2021-10-03 13:19:00 +0900

    :recycle: avoid compile errors

--- Sources/Components/Domain/SpotPost/SpotPostView.swift
+++ Sources/Components/Domain/SpotPost/SpotPostView.swift
@@ -7,27 +7,30 @@ import MapKit
 import FirebaseStorageSwift
 
 struct SpotPostView: View {
+    @Environment(\.presentationMode) var presentationMode
+
     @StateObject var upload = Upload()
     @StateObject var mutation = Mutation<SpotAddMutation>()
 
-    @State var title: String?
-    @State var imageName: String?
     @State var image: UIImage?
+    @State var title: String = ""
+    @State var geoPoint: CLLocationCoordinate2D?
 
-    var body: some View {
-        EmptyView()
+    var submitButtonIsDisabled: Bool {
+        image == nil || title.isEmpty
     }
+
     var body: some View {
         NavigationView {
             ZStack(alignment: .top) {
                 Color.screenBackground.edgesIgnoringSafeArea(.all)
                 ScrollView(showsIndicators: false) {
                     VStack(spacing: 18) {
-                        SpotPostImageView()
+                        SpotPostImageView(image: $image)
                         VStack(alignment: .leading) {
                             Text("タイトル")
                                 .font(.subheadline)
-                            TextField("ポスターのタイトル", text: viewStore.binding(get: \.viewState.title, send: SpotPostAction.edited(title:)))
+                            TextField("ポスターのタイトル", text: $title)
                                 .font(.caption)
                                 .textFieldStyle(PlainTextFieldStyle())
                                 .frame(maxWidth: .infinity)
@@ -41,13 +44,10 @@ struct SpotPostView: View {
                         VStack(alignment: .leading) {
                             Button(
                                 action: {
-                                    if !viewStore.viewState.canEditGeoPoint {
-                                        return
-                                    }
-                                    viewStore.send(.presentLocationSelect)
+                                    // TODO:
                                 },
                                 label: {
-                                    if let geoPoint = viewStore.viewState.geoPoint {
+                                    if let geoPoint = geoPoint {
                                         Text("画像を撮った場所を選んでください")
                                             .font(.subheadline)
                                     } else {
@@ -56,35 +56,37 @@ struct SpotPostView: View {
                                     }
                                 }
                             )
-                            .frame(height: 80)
+                                .frame(height: 80)
                         }
                         .frame(maxWidth: .infinity)
                         .padding(.leading, 20)
 
                         Spacer()
 
-                        Button(action: {
-
-                        },
-                        label: {
-                            Text("保存")
-                                .foregroundColor(.white)
-                                .font(.body)
-                                .fontWeight(.medium)
-                        })
-                        .disabled(viewStore.viewState.submitButtonIsDisabled)
-                        .frame(width: 200, height: 44, alignment: .center)
-                        .background(viewStore.viewState.submitButtonIsDisabled ? Color.disabled.gradient : GradientColor.barn)
+                        Button(
+                            action: {
+                                // TODO:
+                            },
+                            label: {
+                                Text("保存")
+                                    .foregroundColor(.white)
+                                    .font(.body)
+                                    .fontWeight(.medium)
+                            })
+                            .disabled(submitButtonIsDisabled)
+                            .frame(width: 200, height: 44, alignment: .center)
+                            .background(submitButtonIsDisabled ? Color.disabled.gradient : GradientColor.barn
+                            )
 
                         Spacer().frame(height: 32)
                     }
                 }
             }
-            .padding(.vertical: 16)
+            .padding(.vertical, 16)
             .navigationBarItems(
                 leading:
                     Button(action: {
-                        viewStore.send(.dismiss)
+                        presentationMode.wrappedValue.dismiss()
                     }) {
                         Image(systemName: "xmark")
                             .renderingMode(.template)
@@ -93,47 +95,6 @@ struct SpotPostView: View {
             )
             .navigationBarTitle("", displayMode: .inline)
         }
-        .sheet(
-            item: viewStore.binding(
-                get: \.presentationType,
-                send: { .presentationTypeDidChanged($0) }
-            ),
-            content: { type -> AnyView in
-                switch type {
-                case .camera:
-                    return AnyView(
-                        PhotoCameraViewConnector(
-                            store: store.scope(
-                                state: \.photoCamera,
-                                action: { .photoCameraAction($0)}
-                            )
-                        )
-                    )
-                case .photoLibrary:
-                    return AnyView(
-                        PhotoLibraryViewConnector(
-                            store: store.scope(
-                                state: \.photoLibrary,
-                                action: { .photoLibraryAction($0) }
-                            )
-                        )
-                    )
-                case .locationSelection:
-                    return AnyView(
-                        LocationSelectView(
-                            store: store.scope(
-                                state: \.locationSelect,
-                                action: { .locationSelectAction($0) }
-                            )
-                        )
-                    )
-                case .openSettingAlert:
-                    return AnyView(EmptyView())
-                case .notPermissionAlert:
-                    return AnyView(EmptyView())
-                }
-            }
-        )
     }
     
 }
