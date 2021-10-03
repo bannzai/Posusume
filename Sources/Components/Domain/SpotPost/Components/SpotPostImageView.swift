e812da1f593c025506dca2ec3fbd620ad65e9e1e
bannzai <kingkong999yhirose@gmail.com>
2021-10-03 13:12:32 +0900

    :sparkles: add SpotPostImageView

---
+++ Sources/Components/Domain/SpotPost/Components/SpotPostImageView.swift
@@ -0,0 +1,41 @@
+import Foundation
+import SwiftUI
+
+public struct SpotPostImageView: View {
+    @Binding var image: UIImage?
+    @State var showsActionSheet: Bool = false
+
+    public var body: some View {
+        Button (
+            action: {
+                showsActionSheet = true
+            },
+            label: {
+                if let image = image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .frame(width: UIScreen.main.bounds.width - 40)
+                        .aspectRatio(3 / 4, contentMode: .fit)
+                        .clipped()
+                        .padding(.horizontal, 20)
+                } else {
+                    VStack {
+                        Image("anyPicture")
+                            .resizable()
+                            .renderingMode(.template)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                        Text("画像を選択")
+                            .font(.footnote)
+                    }
+                    .foregroundColor(.placeholder)
+                    .frame(maxWidth: .infinity, minHeight: 160, maxHeight: 160)
+                    .background(Color.white)
+                    .padding(.horizontal, 20)
+                }
+            })
+            .buttonStyle(PlainButtonStyle())
+            .adaptImagePickEvent(showsActionSheet: $showsActionSheet)
+    }
+}
+
