72368cddd1a2f7af069e1b92299d909c9ee63501
bannzai <kingkong999yhirose@gmail.com>
2021-10-04 16:18:22 +0900

    :memo: Receive optionals

--- Sources/Domain/Location/Geocoder.swift
+++ Sources/Domain/Location/Geocoder.swift
@@ -20,14 +20,20 @@ private struct _Geocoder: Geocoder {
                         return nil
                     }
                     return Place(
-                        name: mark.name ?? "",
-                        country: mark.country ?? "",
-                        postalCode: mark.postalCode ?? "",
+                        name: mark.name,
+                        country: mark.country,
+                        isoCountryCode: mark.isoCountryCode,
+                        postalCode: mark.postalCode,
+                        inlandWater: mark.inlandWater,
+                        ocean: mark.ocean,
+                        areasOfInterest: mark.areasOfInterest,
                         address: .init(
-                            administrativeArea: mark.administrativeArea ?? "",
-                            locality: mark.locality ?? "",
-                            thoroughfare: mark.thoroughfare ?? "",
-                            subThoroughfare: mark.subThoroughfare ?? ""
+                            administrativeArea: mark.administrativeArea,
+                            subAdministrativeArea: mark.subAdministrativeArea,
+                            locality: mark.locality,
+                            subLocality: mark.subLocality,
+                            thoroughfare: mark.thoroughfare,
+                            subThoroughfare: mark.subThoroughfare
                         ),
                         location: location.coordinate
                     )
@@ -39,29 +45,42 @@ private struct _Geocoder: Geocoder {
 
 public struct Place: Equatable, Identifiable {
     public let id: UUID = .init()
-    public let name: String
-    public let country: String
-    public let postalCode: String
+    public let name: String?
+    public let country: String?
+    public let isoCountryCode: String?
+    public let postalCode: String?
+    public let inlandWater: String?
+    public let ocean: String?
+    public let areasOfInterest: [String]?
     public let address: Address
     public let location: CLLocationCoordinate2D
 
     public struct Address: Equatable  {
         // 東京都
-        public let administrativeArea: String
+        public let administrativeArea: String?
+
+        public let subAdministrativeArea: String?
+
         // 渋谷区
-        public let locality: String
+        public let locality: String?
+
+        // neighborhood, common name, eg. Mission District
+        public let subLocality: String?
+
         // X町 4丁目
-        public let thoroughfare: String
+        public let thoroughfare: String?
         // 1番1号
-        public let subThoroughfare: String
+        public let subThoroughfare: String?
 
         public var address: String {
-            "\(administrativeArea)\(locality)\(thoroughfare)\(subThoroughfare)"
+            [administrativeArea, locality, thoroughfare, subThoroughfare]
+                .compactMap{ $0 }
+                .joined()
         }
     }
 
     func formattedLocationAddress() -> String {
-        if !name.isEmpty {
+        if let name = name, !name.isEmpty {
             return "\(name): \(address.address)"
         }
         return address.address
