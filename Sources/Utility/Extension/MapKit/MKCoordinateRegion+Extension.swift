4c1013a30f27a866ab16191f1dcf837b0ea9f234
bannzai <kingkong999yhirose@gmail.com>
2021-10-08 07:20:33 +0900

    :memo: add utility corner digresses

--- Sources/Utility/Extension/MapKit/MKCoordinateRegion+Extension.swift
+++ Sources/Utility/Extension/MapKit/MKCoordinateRegion+Extension.swift
@@ -13,3 +13,40 @@ extension MKCoordinateRegion: Equatable {
     }
 }
 
+extension MKCoordinateRegion {
+    func cornerDigresses() -> (topLeft: CLLocationCoordinate2D, topRight: CLLocationCoordinate2D, bottomLeft: CLLocationCoordinate2D, bottomRight: CLLocationCoordinate2D) {
+        let lat = convertIfLatitudeDegressReachedBoundary
+        let lon = convertIfLongitudeDegressReachedBoundary
+        let left = lon(center.longitude - span.longitudeDelta / 2)
+        let right = lon(center.longitude + span.longitudeDelta / 2)
+        let top = lat(center.latitude + span.latitudeDelta / 2)
+        let bottom = lat(center.latitude - span.latitudeDelta / 2)
+        return (
+            topLeft: .init(latitude: left, longitude: top),
+            topRight: .init(latitude: right, longitude: top),
+            bottomLeft: .init(latitude: left, longitude: bottom),
+            bottomRight: .init(latitude: right, longitude: bottom)
+        )
+    }
+
+    // NOTE: Latitude is between 90 ~ -90 degrees. Next to 90 is -89 degrees.
+    // Since drawing range of Map UI is not repeated in the vertical direction, So, the maximum value is 90 and the minimum value is -90.
+    // 90 degrees north and -90 degrees south
+    private func convertIfLatitudeDegressReachedBoundary(_ latitude: CLLocationDegrees) -> CLLocationDegrees {
+        min(90, max(-90, latitude))
+    }
+
+    // NOTE: Latitude is between 180 ~ -180 degrees. Next to 180 is -180 degrees.
+    // 180 degrees east and -180 degrees west
+    private func convertIfLongitudeDegressReachedBoundary(_ longitude: CLLocationDegrees) -> CLLocationDegrees {
+        if longitude > 180 {
+            let diff = longitude - 180
+            return -180 + diff
+        }
+        if longitude < -180 {
+            let diff = longitude + 180
+            return 180 - diff
+        }
+        return longitude
+    }
+}
