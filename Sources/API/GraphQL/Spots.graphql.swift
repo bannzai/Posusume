0841a130a8d37302081d0637e0e63ee5dc41bafb
bannzai <kingkong999yhirose@gmail.com>
2021-10-08 08:38:52 +0900

    :memo: Updated spots schema

--- Sources/API/GraphQL/Spots.graphql.swift
+++ Sources/API/GraphQL/Spots.graphql.swift
@@ -8,22 +8,37 @@ public final class SpotsQuery: GraphQLQuery {
   /// The raw GraphQL definition of this operation.
   public let operationDefinition: String =
     """
-    query Spots {
-      me {
+    query Spots($spotsMinLatitude: Latitude!, $spotsMinLongitude: Longitude!, $spotsMaxLatitude: Latitude!, $spotsMaxLongitude: Longitude!) {
+      spots(
+        minLatitude: $spotsMinLatitude
+        minLongitude: $spotsMinLongitude
+        maxLatitude: $spotsMaxLatitude
+        maxLongitude: $spotsMaxLongitude
+      ) {
         __typename
         id
-        spots {
-          __typename
-          id
-          title
-        }
+        title
+        imageURL
       }
     }
     """
 
   public let operationName: String = "Spots"
 
-  public init() {
+  public var spotsMinLatitude: Latitude
+  public var spotsMinLongitude: Longitude
+  public var spotsMaxLatitude: Latitude
+  public var spotsMaxLongitude: Longitude
+
+  public init(spotsMinLatitude: Latitude, spotsMinLongitude: Longitude, spotsMaxLatitude: Latitude, spotsMaxLongitude: Longitude) {
+    self.spotsMinLatitude = spotsMinLatitude
+    self.spotsMinLongitude = spotsMinLongitude
+    self.spotsMaxLatitude = spotsMaxLatitude
+    self.spotsMaxLongitude = spotsMaxLongitude
+  }
+
+  public var variables: GraphQLMap? {
+    return ["spotsMinLatitude": spotsMinLatitude, "spotsMinLongitude": spotsMinLongitude, "spotsMaxLatitude": spotsMaxLatitude, "spotsMaxLongitude": spotsMaxLongitude]
   }
 
   public struct Data: GraphQLSelectionSet {
@@ -31,7 +46,7 @@ public final class SpotsQuery: GraphQLQuery {
 
     public static var selections: [GraphQLSelection] {
       return [
-        GraphQLField("me", type: .object(Me.selections)),
+        GraphQLField("spots", arguments: ["minLatitude": GraphQLVariable("spotsMinLatitude"), "minLongitude": GraphQLVariable("spotsMinLongitude"), "maxLatitude": GraphQLVariable("spotsMaxLatitude"), "maxLongitude": GraphQLVariable("spotsMaxLongitude")], type: .nonNull(.list(.nonNull(.object(Spot.selections))))),
       ]
     }
 
@@ -41,27 +56,28 @@ public final class SpotsQuery: GraphQLQuery {
       self.resultMap = unsafeResultMap
     }
 
-    public init(me: Me? = nil) {
-      self.init(unsafeResultMap: ["__typename": "Query", "me": me.flatMap { (value: Me) -> ResultMap in value.resultMap }])
+    public init(spots: [Spot]) {
+      self.init(unsafeResultMap: ["__typename": "Query", "spots": spots.map { (value: Spot) -> ResultMap in value.resultMap }])
     }
 
-    public var me: Me? {
+    public var spots: [Spot] {
       get {
-        return (resultMap["me"] as? ResultMap).flatMap { Me(unsafeResultMap: $0) }
+        return (resultMap["spots"] as! [ResultMap]).map { (value: ResultMap) -> Spot in Spot(unsafeResultMap: value) }
       }
       set {
-        resultMap.updateValue(newValue?.resultMap, forKey: "me")
+        resultMap.updateValue(newValue.map { (value: Spot) -> ResultMap in value.resultMap }, forKey: "spots")
       }
     }
 
-    public struct Me: GraphQLSelectionSet {
-      public static let possibleTypes: [String] = ["Me"]
+    public struct Spot: GraphQLSelectionSet {
+      public static let possibleTypes: [String] = ["Spot"]
 
       public static var selections: [GraphQLSelection] {
         return [
           GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
           GraphQLField("id", type: .nonNull(.scalar(GraphQLID.self))),
-          GraphQLField("spots", type: .nonNull(.list(.nonNull(.object(Spot.selections))))),
+          GraphQLField("title", type: .nonNull(.scalar(String.self))),
+          GraphQLField("imageURL", type: .nonNull(.scalar(URL.self))),
         ]
       }
 
@@ -71,8 +87,8 @@ public final class SpotsQuery: GraphQLQuery {
         self.resultMap = unsafeResultMap
       }
 
-      public init(id: GraphQLID, spots: [Spot]) {
-        self.init(unsafeResultMap: ["__typename": "Me", "id": id, "spots": spots.map { (value: Spot) -> ResultMap in value.resultMap }])
+      public init(id: GraphQLID, title: String, imageUrl: URL) {
+        self.init(unsafeResultMap: ["__typename": "Spot", "id": id, "title": title, "imageURL": imageUrl])
       }
 
       public var __typename: String {
@@ -93,61 +109,21 @@ public final class SpotsQuery: GraphQLQuery {
         }
       }
 
-      public var spots: [Spot] {
+      public var title: String {
         get {
-          return (resultMap["spots"] as! [ResultMap]).map { (value: ResultMap) -> Spot in Spot(unsafeResultMap: value) }
+          return resultMap["title"]! as! String
         }
         set {
-          resultMap.updateValue(newValue.map { (value: Spot) -> ResultMap in value.resultMap }, forKey: "spots")
+          resultMap.updateValue(newValue, forKey: "title")
         }
       }
 
-      public struct Spot: GraphQLSelectionSet {
-        public static let possibleTypes: [String] = ["Spot"]
-
-        public static var selections: [GraphQLSelection] {
-          return [
-            GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
-            GraphQLField("id", type: .nonNull(.scalar(GraphQLID.self))),
-            GraphQLField("title", type: .nonNull(.scalar(String.self))),
-          ]
+      public var imageUrl: URL {
+        get {
+          return resultMap["imageURL"]! as! URL
         }
-
-        public private(set) var resultMap: ResultMap
-
-        public init(unsafeResultMap: ResultMap) {
-          self.resultMap = unsafeResultMap
-        }
-
-        public init(id: GraphQLID, title: String) {
-          self.init(unsafeResultMap: ["__typename": "Spot", "id": id, "title": title])
-        }
-
-        public var __typename: String {
-          get {
-            return resultMap["__typename"]! as! String
-          }
-          set {
-            resultMap.updateValue(newValue, forKey: "__typename")
-          }
-        }
-
-        public var id: GraphQLID {
-          get {
-            return resultMap["id"]! as! GraphQLID
-          }
-          set {
-            resultMap.updateValue(newValue, forKey: "id")
-          }
-        }
-
-        public var title: String {
-          get {
-            return resultMap["title"]! as! String
-          }
-          set {
-            resultMap.updateValue(newValue, forKey: "title")
-          }
+        set {
+          resultMap.updateValue(newValue, forKey: "imageURL")
         }
       }
     }
