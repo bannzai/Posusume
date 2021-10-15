84bc237a1c60466f8237caead0d6291922df4325
bannzai <kingkong999yhirose@gmail.com>
2021-10-16 08:28:34 +0900

    :memo: fix indent

--- scripts/apollo/ApolloCodegen/Sources/ApolloCodegen/main.swift
+++ scripts/apollo/ApolloCodegen/Sources/ApolloCodegen/main.swift
@@ -4,15 +4,17 @@ print("Launched ApolloCodegen process...")
 
 let cwd = URL(string: "file://\(FileManager.default.currentDirectoryPath)")!
 print("cwd: ", cwd)
+
 let cliPath = cwd.appendingPathComponent("ApolloCLI")
 print("ApolloCodegen CLI binary path of \(cliPath.absoluteString)")
+
 let schemaPath = cwd.appendingPathComponent("schema.graphqls")
 print("ApolloCodegen will download and use schema path of \(schemaPath.absoluteString)")
+
 let appPath = cwd // ApolloCodegen
     .deletingLastPathComponent() // apollo
     .deletingLastPathComponent() // scripts
     .deletingLastPathComponent() // Posusume
-
 print("Defined application path: \(appPath)")
 
 do {
