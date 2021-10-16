71c90d021209cc3c9d0c2772f0dfb063693bfa50
bannzai <kingkong999yhirose@gmail.com>
2021-10-17 08:54:25 +0900

    :sparkels: fix spot detail page

--- Sources/Domain/SpotDetail/SpotDetailPage.swift
+++ Sources/Domain/SpotDetail/SpotDetailPage.swift
@@ -5,41 +5,51 @@ import Apollo
 public struct SpotDetailPage: View {
     @Environment(\.dismiss) var dismiss
 
+    @StateObject var cache = Cache<SpotQuery>()
+    @StateObject var query = Query<SpotQuery>()
+
+    @State var response: SpotQuery.Data?
+    @State var error: Error?
+
     let spotID: String
 
     public var body: some View {
         NavigationView {
-            GeometryReader { geometry in
-                ScrollView(showsIndicators: false) {
-                    VStack(spacing: 20) {
-                        SpotPostImage(
-                            width: geometry.size.width,
-                            image: image,
-                            takenPhoto: takenPhoto,
-                            selectedPhoto: selectedPhoto
-                        )
-                        SpotPostTitle(title: $title)
-                        if image != nil {
-                            SpotPostGeoPoint(place: $placemark)
+            Loading(value: response) { response in
+                GeometryReader { geometry in
+                    ScrollView(showsIndicators: false) {
+                        VStack(spacing: 20) {
+                            SpotDetailImage(
+                                width: geometry.size.width,
+                                fragment: response.spot.fragments.spotDetailImageFragment
+                            )
                         }
-                        Spacer()
-
-                        SpotPostSubmitButton(image: image, title: title, placemark: placemark, dismiss: dismiss)
+                        .padding(.vertical, 24)
                     }
-                    .padding(.vertical, 24)
                 }
+                .navigationBarItems(
+                    leading: Button(action: dismiss.callAsFunction, label: {
+                        Image(systemName: "xmark")
+                            .renderingMode(.template)
+                            .foregroundColor(.appPrimary)
+                    })
+                )
+                .navigationBarTitle(response.spot.title, displayMode: .inline)
             }
             .frame(maxWidth: .infinity)
             .padding(.horizontal, 20)
             .background(Color.screenBackground.edgesIgnoringSafeArea(.all))
-            .navigationBarItems(
-                leading: Button(action: dismiss.callAsFunction, label: {
-                    Image(systemName: "xmark")
-                        .renderingMode(.template)
-                        .foregroundColor(.appPrimary)
-                })
-            )
-            .navigationBarTitle("", displayMode: .inline)
+            .task {
+                if let response = await cache(for: .init(spotId: spotID)) {
+                    self.response = response
+                }
+
+                do {
+                    response = try await query(for: .init(spotId: spotID))
+                } catch {
+                    self.error = error
+                }
+            }
         }
     }
 }
