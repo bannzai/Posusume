a26dcbcde7ecbf79abca917b09db98920f820349
bannzai <kingkong999yhirose@gmail.com>
2021-10-17 08:07:30 +0900

    :recycle: Using sheet

--- Sources/Domain/Spot/Components/SpotMapImage.swift
+++ Sources/Domain/Spot/Components/SpotMapImage.swift
@@ -4,23 +4,25 @@ import SwiftUI
 struct SpotMapImage: View {
     let fragment: SpotMapImageFragment
 
+    @State var isPresentingSpotDetail: Bool = false
+
     var body: some View {
-        NavigationLink(
-            destination: {
-                SpotDetailPage(spotID: fragment.id)
-            },
-            label: {
-                AsyncImage(url: fragment.resizedSpotImageUrLs.thumbnail ?? fragment.imageUrl) { image in
-                    image
-                        .resizable()
-                        .scaledToFill()
-                } placeholder: {
-                    ProgressView()
-                }
-                .frame(width: 40, height: 40)
-                .clipShape(Circle())
-                .overlay(Circle().stroke(GradientColor.barn, lineWidth: 1))
-            }
-        )
+        AsyncImage(url: imageURL) { image in
+            image
+                .resizable()
+                .scaledToFill()
+        } placeholder: {
+            ProgressView()
+        }
+        .frame(width: 40, height: 40)
+        .clipShape(Circle())
+        .overlay(Circle().stroke(GradientColor.barn, lineWidth: 1))
+        .sheet(isPresented: $isPresentingSpotDetail) {
+            SpotDetailPage(spotID: fragment.id)
+        }
+    }
+
+    private var imageURL: URL {
+        fragment.resizedSpotImageUrLs.thumbnail ?? fragment.imageUrl
     }
 }
