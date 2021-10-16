19ccf5117848c854dac49d6c2a7f43ec5a2908ab
bannzai <kingkong999yhirose@gmail.com>
2021-10-17 07:52:38 +0900

    :sparkles: add wire to spot detail page

--- Sources/Domain/Spot/Components/SpotMapImage.swift
+++ Sources/Domain/Spot/Components/SpotMapImage.swift
@@ -5,15 +5,22 @@ struct SpotMapImage: View {
     let fragment: SpotMapImageFragment
 
     var body: some View {
-        AsyncImage(url: fragment.resizedSpotImageUrLs.thumbnail ?? fragment.imageUrl) { image in
-            image
-                .resizable()
-                .scaledToFill()
-        } placeholder: {
-            ProgressView()
-        }
-        .frame(width: 40, height: 40)
-        .clipShape(Circle())
-        .overlay(Circle().stroke(GradientColor.barn, lineWidth: 1))
+        NavigationLink(
+            destination: {
+                SpotDetailPage()
+            },
+            label: {
+                AsyncImage(url: fragment.resizedSpotImageUrLs.thumbnail ?? fragment.imageUrl) { image in
+                    image
+                        .resizable()
+                        .scaledToFill()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: 40, height: 40)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(GradientColor.barn, lineWidth: 1))
+            }
+        )
     }
 }
---
+++ Sources/Domain/SpotDetail/SpotDetailPage.swift
@@ -0,0 +1,8 @@
+import Foundation
+import SwiftUI
+
+public struct SpotDetailPage: View {
+    public var body: some View {
+        Text("Hello world")
+    }
+}
