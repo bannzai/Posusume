96c16fca858dc8936d30c519f0dbb082d07a6a36
bannzai <kingkong999yhirose@gmail.com>
2021-10-17 08:27:53 +0900

    :sparkles: extract to spot image frame modifier

---
+++ Sources/Components/ViewModifier/SpotImageFrameModifier.swift
@@ -0,0 +1,20 @@
+import Foundation
+import SwiftUI
+
+public struct SpotImageFrameModifier: ViewModifier {
+    // NOTE: The size of the ScrollView will not be determined unless the size of the child(SpotPostImage) element is determined. `width` for determining the child size
+    // And it is difficult to use GeometryReader inside ScrollView
+    // See also: https://stackoverflow.com/questions/58965503/geometryreader-in-swiftui-scrollview-causes-weird-behaviour-and-random-offset
+    let width: CGFloat
+    public func body(content: Content) -> some View {
+        content
+            .scaledToFill()
+            .frame(width: width, height: width / 3 * 4)
+    }
+}
+
+extension View {
+    public func spotImageFrame(width: CGFloat) -> some View {
+        modifier(spotImageFrame(width: width))
+    }
+}
--- Sources/Domain/SpotDetail/SpotDetailPage.swift
+++ Sources/Domain/SpotDetail/SpotDetailPage.swift
@@ -3,9 +3,43 @@ import SwiftUI
 import Apollo
 
 public struct SpotDetailPage: View {
+    @Environment(\.dismiss) var dismiss
+
     let spotID: String
 
     public var body: some View {
-        Text("Hello world")
+        NavigationView {
+            GeometryReader { geometry in
+                ScrollView(showsIndicators: false) {
+                    VStack(spacing: 20) {
+                        SpotPostImage(
+                            width: geometry.size.width,
+                            image: image,
+                            takenPhoto: takenPhoto,
+                            selectedPhoto: selectedPhoto
+                        )
+                        SpotPostTitle(title: $title)
+                        if image != nil {
+                            SpotPostGeoPoint(place: $placemark)
+                        }
+                        Spacer()
+
+                        SpotPostSubmitButton(image: image, title: title, placemark: placemark, dismiss: dismiss)
+                    }
+                    .padding(.vertical, 24)
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.horizontal, 20)
+            .background(Color.screenBackground.edgesIgnoringSafeArea(.all))
+            .navigationBarItems(
+                leading: Button(action: dismiss.callAsFunction, label: {
+                    Image(systemName: "xmark")
+                        .renderingMode(.template)
+                        .foregroundColor(.appPrimary)
+                })
+            )
+            .navigationBarTitle("", displayMode: .inline)
+        }
     }
 }
