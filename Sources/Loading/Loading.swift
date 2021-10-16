f39d972c289a18b321b6a3c585444c580d95dfb7
bannzai <kingkong999yhirose@gmail.com>
2021-10-17 08:50:41 +0900

    :sparkles: add Loading

---
+++ Sources/Loading/Loading.swift
@@ -0,0 +1,15 @@
+import Foundation
+import SwiftUI
+
+public struct Loading<Content: View, Value>: View {
+    let value: Value?
+    @ViewBuilder let content: (Value) -> Content
+
+    public var body: some View {
+        if let value = value {
+            content(value)
+        } else {
+            ProgressView("Now Loading...")
+        }
+    }
+}
