cc245d86a8fac784d3739b83506a1c52b1038325
bannzai <kingkong999yhirose@gmail.com>
2021-10-24 07:00:15 +0900

    :recycle: wrap with NavigationView

--- Sources/Domain/SpotPostEditor/SpotPostEditorPage.swift
+++ Sources/Domain/SpotPostEditor/SpotPostEditorPage.swift
@@ -12,49 +12,51 @@ public struct SpotPostEditorPage: View {
     @FocusState var textFieldIsFocused: Bool
 
     public var body: some View {
-        VStack {
-            GeometryReader { geometry in
-                ZStack {
-                    Image(uiImage: image)
-                        .resizable()
-                        .clipped()
+        NavigationView {
+            VStack {
+                GeometryReader { geometry in
+                    ZStack {
+                        Image(uiImage: image)
+                            .resizable()
+                            .clipped()
 
-                    SpotPostEditorEffectCover(
-                        textFieldValues: $textFieldValues,
-                        selectedTextFieldValueID: $selectedTextFieldValueID,
-                        textFieldIsFocused: $textFieldIsFocused
-                    )
-                }.spotImageFrame(width: geometry.size.width)
-            }
-            .onTapGesture {
-                textFieldIsFocused = false
-                selectedTextFieldValueID = nil
-            }
+                        SpotPostEditorEffectCover(
+                            textFieldValues: $textFieldValues,
+                            selectedTextFieldValueID: $selectedTextFieldValueID,
+                            textFieldIsFocused: $textFieldIsFocused
+                        )
+                    }.spotImageFrame(width: geometry.size.width)
+                }
+                .onTapGesture {
+                    textFieldIsFocused = false
+                    selectedTextFieldValueID = nil
+                }
 
-            ScrollView(.horizontal) {
-                HStack {
-                    if let selectedTextFieldIndex = selectedTextFieldIndex {
-                        TextFieldComponentModifiers(textFieldValue: .init(get: {
-                            textFieldValues[selectedTextFieldIndex]
-                        }, set: {
-                            textFieldValues[selectedTextFieldIndex] = $0
-                        }), onDelete: {
-                            textFieldValues.remove(at: selectedTextFieldIndex)
-                        })
-                    } else {
-                        Image(systemName: "textformat")
-                            .font(.system(size: 32))
-                            .frame(width: 40, height: 40)
-                            .onTapGesture {
-                                let element = TextFieldComponentValue(text: "Hello, world")
-                                textFieldValues.append(element)
-                                selectedTextFieldValueID = element.id
-                            }
+                ScrollView(.horizontal) {
+                    HStack {
+                        if let selectedTextFieldIndex = selectedTextFieldIndex {
+                            TextFieldComponentModifiers(textFieldValue: .init(get: {
+                                textFieldValues[selectedTextFieldIndex]
+                            }, set: {
+                                textFieldValues[selectedTextFieldIndex] = $0
+                            }), onDelete: {
+                                textFieldValues.remove(at: selectedTextFieldIndex)
+                            })
+                        } else {
+                            Image(systemName: "textformat")
+                                .font(.system(size: 32))
+                                .frame(width: 40, height: 40)
+                                .onTapGesture {
+                                    let element = TextFieldComponentValue(text: "Hello, world")
+                                    textFieldValues.append(element)
+                                    selectedTextFieldValueID = element.id
+                                }
+                        }
                     }
                 }
             }
+            .padding()
         }
-        .padding()
     }
 
     private var selectedTextFieldIndex: Int? {
