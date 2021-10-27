a8c0e199175e01c821a10606ce0bb12dbdf7e882
bannzai <kingkong999yhirose@gmail.com>
2021-10-27 13:07:14 +0900

    :recycle: Fix firebase configure function

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -5,7 +5,7 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         setupAppearance()
         if !isPreview {
-            setupFirebase()
+            FirebaseApp.configure()
         }
         return true
     }
@@ -20,14 +20,6 @@ func setupAppearance() {
     UINavigationBar.appearance().standardAppearance = appearance
     UINavigationBar.appearance().scrollEdgeAppearance = appearance
 
-}
-
-private func setupFirebase() {
-    #if DEBUG
-    FirebaseApp.configure(options: FirebaseOptions(contentsOfFile: Bundle.main.path(forResource: "GoogleService-Info-dev", ofType: "plist")!)!)
-    #else
-    FirebaseApp.configure(options: FirebaseOptions(contentsOfFile: Bundle.main.path(forResource: "GoogleService-Info-prod", ofType: "plist")!)!)
-    #endif
 }
 
 let isPreview = ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
