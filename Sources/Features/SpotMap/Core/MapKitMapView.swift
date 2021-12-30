29d74d17261399d122dcad9e2db7f1790f2792b0
bannzai <kingkong999yhirose@gmail.com>
2021-12-31 02:23:02 +0900

    :recycle: WIP define mapkit mapview

--- Sources/Features/SpotMap/Core/MapKitMapView.swift
+++ Sources/Features/SpotMap/Core/MapKitMapView.swift
@@ -1,3 +1,32 @@
 import Foundation
+import SwiftUI
 import MapKit
+
+public struct MapKitMapView: UIViewRepresentable {
+    public typealias UIViewType = MKMapView
+
+    public func makeUIView(context: Context) -> MKMapView {
+        let view = MKMapView()
+        view.delegate = context.coordinator
+        return view
+    }
+
+    public func updateUIView(_ uiView: MKMapView, context: Context) {
+
+    }
+
+    public class Coordinator: NSObject, MKMapViewDelegate {
+        let view: MapKitMapView
+
+        init(view: MapKitMapView) {
+            self.view = view
+        }
+    }
+
+    public func makeCoordinator() -> Coordinator {
+        .init(view: self)
+    }
+}
+
+
 
