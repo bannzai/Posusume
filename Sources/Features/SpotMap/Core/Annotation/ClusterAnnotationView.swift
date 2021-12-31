9b11eb6933dd86e8a29a00a43c24f4a6aa2730a2
bannzai <kingkong999yhirose@gmail.com>
2021-12-31 13:22:02 +0900

    :recycle: fix clustring view

--- Sources/Features/SpotMap/Core/Annotation/ClusterAnnotationView.swift
+++ Sources/Features/SpotMap/Core/Annotation/ClusterAnnotationView.swift
@@ -1,10 +1,25 @@
 import MapKit
 
 final class ClusterAnnotationView: MKAnnotationView {
+    let spotCountLabel = UILabel()
+
     override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
         super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+
         collisionMode = .circle
         centerOffset = CGPoint(x: 0, y: -10) // Offset center point to animate better with marker annotations
+
+        addSubview(spotCountLabel)
+        spotCountLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            spotCountLabel.widthAnchor.constraint(equalToConstant: 72),
+            spotCountLabel.heightAnchor.constraint(equalToConstant: 101),
+            spotCountLabel.topAnchor.constraint(equalTo: topAnchor),
+            spotCountLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            spotCountLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            spotCountLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -14,22 +29,10 @@ final class ClusterAnnotationView: MKAnnotationView {
         super.prepareForDisplay()
 
         if let cluster = annotation as? MKClusterAnnotation {
-            let totalBikes = cluster.memberAnnotations.count
+            let spotAnnotations = cluster.memberAnnotations.compactMap { $0 as? SpotMapImageAnnotation }
+            let spotCount = spotAnnotations.count
 
-            
-
-            if count(cycleType: .unicycle) > 0 {
-                image = drawUnicycleCount(count: totalBikes)
-            } else {
-                let tricycleCount = count(cycleType: .tricycle)
-                image = drawRatioBicycleToTricycle(tricycleCount, to: totalBikes)
-            }
-
-            if count(cycleType: .unicycle) > 0 {
-                displayPriority = .defaultLow
-            } else {
-                displayPriority = .defaultHigh
-            }
+            spotCountLabel.text = "\(spotCount)"
         }
     }
 }
