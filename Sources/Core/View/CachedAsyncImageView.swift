f8bebb0cf53635671a4e10f279651cd5971ec6b7
bannzai <kingkong999yhirose@gmail.com>
2022-01-01 14:33:27 +0900

    :memo: fix cached async image view

--- Sources/Core/View/CachedAsyncImageView.swift
+++ Sources/Core/View/CachedAsyncImageView.swift
@@ -13,28 +13,41 @@ private var urlSession: URLSession = {
 }()
 
 public struct CachedAsyncImageView<Content: View>: View {
-    @State var phase: SwiftUI.AsyncImagePhase = .empty
+    @State var phase: SwiftUI.AsyncImagePhase
 
     let url: URL?
-    let urlSession: URLSession
     let scale: CGFloat
     let transaction: Transaction
     let content: (SwiftUI.AsyncImagePhase) -> Content
 
-    public init(url: URL?, urlCache: URLCache = .shared, cachePolicty: NSURLRequest.CachePolicy = .returnCacheDataElseLoad, scale: CGFloat = 1, transaction: Transaction = Transaction(), @ViewBuilder content: @escaping (SwiftUI.AsyncImagePhase) -> Content) {
-        let configuration = URLSessionConfiguration.default
-        configuration.urlCache = urlCache
-        configuration.requestCachePolicy = cachePolicty
+    // Use on network request or cache key
+    static func request(url: URL) -> URLRequest {
+        .init(url: url)
+    }
 
+    public init(url: URL?, scale: CGFloat = 1, transaction: Transaction = Transaction(), @ViewBuilder content: @escaping (SwiftUI.AsyncImagePhase) -> Content) {
         self.url = url
-        self.urlSession = .init(configuration: configuration)
         self.scale = scale
         self.transaction = transaction
         self.content = content
+
+        let phase: SwiftUI.AsyncImagePhase = {
+            guard let url = url else {
+                return .empty
+            }
+
+            if let cachedResponse = urlSession.configuration.urlCache?.cachedResponse(for: Self.request(url: url)) {
+                if let uiImage = UIImage(data: cachedResponse.data) {
+                    return .success(Image(uiImage: uiImage))
+                }
+            }
+            return .empty
+        }()
+        self._phase = .init(initialValue: phase)
     }
 
-    public init<I: View, P: View>(url: Foundation.URL?, urlCache: URLCache = .shared, cachePolicty: NSURLRequest.CachePolicy = .returnCacheDataElseLoad, scale: CoreGraphics.CGFloat = 1, @ViewBuilder content: @escaping (Image) -> I, @ViewBuilder placeholder: @escaping () -> P) where Content == _ConditionalContent<I, P> {
-        self.init(url: url, urlCache: urlCache, cachePolicty: cachePolicty, scale: scale) { phase in
+    public init<I: View, P: View>(url: Foundation.URL?, scale: CoreGraphics.CGFloat = 1, @ViewBuilder content: @escaping (Image) -> I, @ViewBuilder placeholder: @escaping () -> P) where Content == _ConditionalContent<I, P> {
+        self.init(url: url, scale: scale) { phase in
             if let image = phase.image {
                 content(image)
             } else {
@@ -61,12 +74,11 @@ public struct CachedAsyncImageView<Content: View>: View {
             return
         }
 
+        let request = Self.request(url: url)
         do {
-            let request = URLRequest(url: url)
             let (data, _) = try await urlSession.data(for: request)
             if let uiImage = UIImage(data: data) {
-                let image = Image(uiImage: uiImage)
-                phase = .success(image)
+                phase = .success(Image(uiImage: uiImage))
             } else {
                 throw NetworkError()
             }
