d806281cf6abd90a86a8f3d3b436539971df5f9d
bannzai <kingkong999yhirose@gmail.com>
2022-01-05 00:00:42 +0900

    :recycle: fix continuation

--- Sources/Emoist/CloudVisionLabelDetection.swift
+++ Sources/Emoist/CloudVisionLabelDetection.swift
@@ -16,6 +16,7 @@ public struct CloudVision {
     public enum LabelDetectionError: Error {
         case imageCannotConvertToJPEG
         case emotionaliseError(domain: String, errorCode: Int, localizedDescription: String, firebaseFunctionErrorDetails: String?)
+        case labelDecodedError(rawData: Any?)
     }
 
     public func labelDetection(uiImage image: UIImage) async throws ->  [Label] {
@@ -40,21 +41,21 @@ public struct CloudVision {
                         )
                     )
                 } else {
-                    if let responses = result?.data as? [[String: Any]] {
-                        // data/responses is a key of "responses". https://cloud.google.com/vision/docs/labels
-                        if let labelAnnotations = responses.first?["labelAnnotations"] as? [[String: Any]] {
-                            let labels: [Label] = labelAnnotations.compactMap {
-                                guard let mid = $0["mid"] as? String,
-                                      let description = $0["description"] as? String,
-                                      let score = $0["score"] as? Double,
-                                      let topicality = $0["topicality"] as? Double
-                                else {
-                                    return nil
-                                }
-                                return Label(mid: mid, description: description, score: score, topicality: topicality)
+                    // data/responses is a key of "responses". https://cloud.google.com/vision/docs/labels
+                    if let responses = result?.data as? [[String: Any]], let labelAnnotations = responses.first?["labelAnnotations"] as? [[String: Any]] {
+                        let labels: [Label] = labelAnnotations.compactMap {
+                            guard let mid = $0["mid"] as? String,
+                                  let description = $0["description"] as? String,
+                                  let score = $0["score"] as? Double,
+                                  let topicality = $0["topicality"] as? Double
+                            else {
+                                return nil
                             }
-                            continuation.resume(returning: labels)
+                            return Label(mid: mid, description: description, score: score, topicality: topicality)
                         }
+                        continuation.resume(returning: labels)
+                    } else {
+                        continuation.resume(throwing: LabelDetectionError.labelDecodedError(rawData: result?.data))
                     }
                 }
             }
