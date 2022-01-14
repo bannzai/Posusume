08f1ce0c8b66d86aec70420e608d0315f1378656
bannzai <kingkong999yhirose@gmail.com>
2022-01-14 23:35:18 +0900

    :sparkles: add TODO comment

--- Sources/Features/Account/AccountPage.swift
+++ Sources/Features/Account/AccountPage.swift
@@ -3,6 +3,7 @@ import SwiftUI
 
 public struct AccountPage: View {
     public var body: some View {
+        // TODO: Implement uplaod user prfoile and edit user name
         EmptyView()
     }
 }
