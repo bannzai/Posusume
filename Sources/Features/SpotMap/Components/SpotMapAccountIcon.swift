4879acabdab9246995455c08c79d4de078c2ade0
bannzai <kingkong999yhirose@gmail.com>
2022-01-14 23:35:28 +0900

    :sparkles: WIP implement account icon view

--- Sources/Features/SpotMap/Components/SpotMapAccountIcon.swift
+++ Sources/Features/SpotMap/Components/SpotMapAccountIcon.swift
@@ -3,14 +3,33 @@ import SwiftUI
 
 struct SpotMapAccountIcon: View {
     @Environment(\.me) var me
+    @StateObject var query = Query<SpotMapIconQuery>()
+    @StateObject var cache = Cache<SpotMapIconQuery>()
+    @State var user: SpotMapIconQuery.Data.Me.User?
 
     var body: some View {
         NavigationLink {
             AccountPage()
         } label: {
-            Image(systemName: "person.crop.circle")
-                .resizable()
-                .frame(width: 44, height: 44)
+            AsyncImage(url: imageURL) { phase in
+                switch phase {
+                case let .success(image):
+                    image
+                        .resizable()
+                case _:
+                    Image(systemName: "person.crop.circle")
+                        .resizable()
+                }
+            }
+            .frame(width: 44, height: 44)
         }
+        .task {
+            user = await cache(for: .init())?.me.user
+            user = try? await query(for: .init()).me.user
+        }
+    }
+
+    private var imageURL: URL? {
+        user?.resizedProfileImageUrLs.thumbnail ?? user?.profileImageUrl
     }
 }
