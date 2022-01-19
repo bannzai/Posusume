82688cc8269c5c392e9cea5ae8e11459a0918374
bannzai <kingkong999yhirose@gmail.com>
2022-01-19 22:53:17 +0900

    :recycle: fix directories

--- Package/Package.swift
+++ Package/Package.swift
@@ -13,21 +13,18 @@ let package = Package(
         .library(name: "Auth", targets: ["Auth"]),
         .library(name: "CloudStorage", targets: ["CloudStorage"]),
         .library(name: "Components", targets: ["Components"]),
-        .library(name: "Core", targets: ["Core"]),
         .library(name: "Error", targets: ["Error"]),
         .library(name: "Features", targets: ["Features"]),
         .library(name: "GraphQL", targets: ["GraphQL"]),
         .library(name: "Resource", targets: ["Resource"]),
         .library(name: "StdLib", targets: ["StdLib"]),
-        .library(name: "Style", targets: ["Style"]),
+        .library(name: "Style", targets: ["Style"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk", from: "8.10.0"),
+        .package(name: "Apollo", url: "https://github.com/apollographql/apollo-ios", from: "0.50.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Package",
             dependencies: []),
