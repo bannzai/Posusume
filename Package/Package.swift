e2644352c8f0a592ca7bd0f2213cd07522d83d72
bannzai <kingkong999yhirose@gmail.com>
2022-01-20 22:17:56 +0900

    :memo: fix package manifest

--- Package/Package.swift
+++ Package/Package.swift
@@ -12,30 +12,51 @@ let package = Package(
         .library(name: "Application", targets: ["Application"]),
     ],
     dependencies: [
-        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk", from: "8.10.0"),
-        .package(name: "Apollo", url: "https://github.com/apollographql/apollo-ios", from: "0.50.0")
+        .package(name: "Apollo", url: "https://github.com/apollographql/apollo-ios", .upToNextMinor(from: "0.49.0")),
+        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMinor(from: "8.10.0")),
     ],
     targets: [
-        .target(name: "Account", dependencies: []),
-        .target(name: "Application", dependencies: []),
-        .target(name: "Auth", dependencies: []),
-        .target(name: "CloudStorage", dependencies: []),
+        // MARK: - Main
+        .target(name: "Application", dependencies: [
+            "Auth",
+        ]),
+
+        // MARK: - Domain
+        .target(name: "Account", dependencies: [
+            "GraphQL",
+        ]),
+        .target(name: "SpotDetail", dependencies: [
+            "GraphQL",
+        ]),
+        .target(name: "SpotMap", dependencies: [
+            "GraphQL",
+        ]),
+        .target(name: "SpotPost", dependencies: [
+            "GraphQL",
+        ]),
+        .target(name: "SpotPostEditor", dependencies: [
+            "GraphQL",
+        ]),
+
+        // MARK: - Library
+        .target(name: "Auth", dependencies: [
+            .product(name: "FirebaseAuth", package: "Firebase"),
+        ]),
+        .target(name: "CloudStorage", dependencies: [
+            .product(name: "FirebaseStorage", package: "Firebase"),
+        ]),
+        .target(name: "GraphQL", dependencies: [
+            .product(name: "Apollo", package: "Apollo"),
+            .product(name: "ApolloSQLite", package: "Apollo"),
+        ]),
+        .target(name: "StdLib", dependencies: []),
+
+        // MARK: - Component
         .target(name: "Components", dependencies: []),
         .target(name: "Error", dependencies: []),
-        .target(name: "GraphQL", dependencies: []),
         .target(name: "Location", dependencies: []),
         .target(name: "Photo", dependencies: []),
         .target(name: "Resource", dependencies: []),
-        .target(name: "SpotDetail", dependencies: []),
-        .target(name: "SpotMap", dependencies: []),
-        .target(name: "SpotPost", dependencies: []),
-        .target(name: "SpotPostEditor", dependencies: []),
-        .target(name: "StdLib", dependencies: []),
         .target(name: "Style", dependencies: []),
-
-
-        .testTarget(
-            name: "PackageTests",
-            dependencies: ["Package"]),
     ]
 )
